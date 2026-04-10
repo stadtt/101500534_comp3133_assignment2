4f4655007c2cccc084d7f830f47cc15da2da45e7
shaffaq <hai.shaffaq@gmail.com>
2026-04-10 13:45:15 -0400

    added skeleton for employee tables

--- frontend/src/app/components/admin-view/admin-view.ts
+++ frontend/src/app/components/admin-view/admin-view.ts
@@ -2,7 +2,6 @@ import { Component, inject, signal } from '@angular/core';
 import { Header } from '../header/header';
 import { EmployeeService } from '../../service/employee.service';
 import { Employee } from '../../models/emplotee.type';
-import { map } from 'rxjs/internal/operators/map';
 
 @Component({
   selector: 'app-admin-view',
@@ -24,6 +23,16 @@ export class AdminView {
         console.error('Error fetching employees:', error);
       }
     });
+  }
+
+  editEmployee(employeeId: number) {
+    console.log('Edit employee with ID:', employeeId);
+    // Implement edit functionality here
+  }
+
+  deleteEmployee(employeeId: number) {
+    console.log('Delete employee with ID:', employeeId);
+    // Implement delete functionality here
   }
 
 }
